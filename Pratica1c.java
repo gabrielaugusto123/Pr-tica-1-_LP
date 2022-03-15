09a87e33668e145b62c99a5f6d2c1fa71ede61d0
gabrielaugusto123 <84480867+gabrielaugusto123@users.noreply.github.com>
2022-03-14 22:19:50 -0300

    Update Pratica1c.java

--- Pratica1c.java
+++ Pratica1c.java
@@ -2,22 +2,37 @@ package br.com.pratica1.git;
 import java.util.Scanner;
 public class Pratica1c {
 	public static void main(String[] args) {
-		Scanner entrada = new Scanner (System.in);
-	      
-		int vogal = 0;
+
 		
-        System.out.println("Digite uma frase: ");            
-        String frase = entrada.nextLine();
-        
-        frase = frase.toLowerCase();
-        for(int i = 0; i < frase.length(); i++){
-            char c = frase.charAt(i);
-            if(c == 32) continue;
-            if(c == 97 | c == 101 | c == 105 | c == 111 | c == 117){
-                vogal++;
-                continue;
-            }
-        }
-        System.out.println("Na frase \"" + frase + "\" temos: " + vogal + " vogais.");
-}
-}
+		
+				Scanner entrada = new Scanner (System.in);
+			      
+			    int cnt=0;
+				String vogais= "";
+				
+		        System.out.println("Digite uma frase: ");            
+		        String frase = entrada.nextLine();
+		        
+		        frase = frase.toLowerCase();
+		        for(int i = 0; i < frase.length(); i++){
+		            char c = frase.charAt(i);
+		            
+		            if(c == 'a' | c == 'e' | c == 'i' | c == 'o' | c == 'u'){
+		               
+		               vogais= vogais+Character.toString(c);
+		               
+		            }
+		        }
+		        if(vogais.contains("a"))
+		        	cnt++;
+		        if(vogais.contains("e"))
+		        	cnt++;
+		        if(vogais.contains("i"))
+		        	cnt++;
+		        if(vogais.contains("o"))
+		        	cnt++;
+		        if(vogais.contains("u"))
+	        	cnt++;
+		        System.out.println("Na frase \"" + frase + "\" temos: " + cnt  + " vogais.");
+		}
+		}
